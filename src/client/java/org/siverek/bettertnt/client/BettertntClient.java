91452ce5e0577a5e3e0c018ff80b68b7b4880ec0
Asaf31214 <asaf.kanlipicak@outlook.com>
2024-10-27 08:33:11 +0300

    project initiation and suspicious substance item added to the game

---
+++ src/client/java/org/siverek/bettertnt/client/BettertntClient.java
@@ -0,0 +1,11 @@
+package org.siverek.bettertnt.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BettertntClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/siverek/bettertnt/Bettertnt.java
@@ -0,0 +1,18 @@
+package org.siverek.bettertnt;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bettertnt implements ModInitializer {
+
+    public static final String MOD_ID = "bettertnt";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+    @Override
+    public void onInitialize() {
+        ModItems.initialize();
+        LOGGER.info("Hello Fabric world!");
+    }
+}
